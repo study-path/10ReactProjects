548af78766ccf5c6bfa1710395fba401ff40e4be
LiliyaIngul <liliyaingul@gmail.com>
2020-11-10 01:07:35 -0500

    init commit

---
+++ 3_review/src/App.js
@@ -0,0 +1,19 @@
+import Review from './Review';
+
+function App() {
+  return (
+    <main>
+      <div className="container">
+        <div className="title">
+        <h2>Our Reviews</h2> 
+        </div>
+        <div className='underLine'></div>
+        <Review />     
+      </div>
+
+
+    </main>
+  );
+}
+
+export default App;
